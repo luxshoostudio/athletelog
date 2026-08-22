e787f88fee735ccbdb5df9e162e0d07bbade20f7
Lux Shoo <luxshoo@foxmail.com>
2026-08-21 17:00:56 -0700

    启动页加点击交互：点任意处立即进入，自动跳留作兜底
    - welcome 页 bindtap 全屏可点，点击立即 redirectTo 今日
    - 卡片加按压反馈 hover-class；底部加「点击进入」呼吸提示
    - 自动跳从 1.2s 延到 1.6s；goToday 加防重复跳转 + onUnload 清定时器

--- miniprogram/pages/welcome/index.js
+++ miniprogram/pages/welcome/index.js
@@ -1,7 +1,28 @@
 Page({
   onLoad: function () {
-    setTimeout(function () {
-      wx.redirectTo({ url: '/pages/today/index' });
-    }, 1200);
+    const that = this;
+    this._entered = false;
+    this._timer = setTimeout(function () {
+      that.goToday();
+    }, 1600);
+  },
+
+  onUnload: function () {
+    if (this._timer) {
+      clearTimeout(this._timer);
+    }
+  },
+
+  goToday: function () {
+    if (this._entered) return;
+    this._entered = true;
+    if (this._timer) {
+      clearTimeout(this._timer);
+    }
+    wx.redirectTo({ url: '/pages/today/index' });
+  },
+
+  enter: function () {
+    this.goToday();
   }
 });
